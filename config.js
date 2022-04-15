e08b6eb9ec7d147df76a6ce33716a1051290ab11
Full-Stack & Blockchain Developer <54260086+MigelTom@users.noreply.github.com>
2022-04-16 08:52:43 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+  export const nftmarketaddress = "0xCb3117819636E75437937C60A436E1C39Bd192B6"
+  export const nftaddress = "0xCB65ea2F9a16cA5D3adeFdE16c037001B355eb31"
+  export const rpcEndpoint = "https://ropsten.infura.io/v3/c74a1d1c7dab4b15853047289874e106"
